5853704fbb43cd8b9bdd6ef9ef33618af6d82113
sirichandana24 <sirichandana24@missionrnd.com>
2016-04-08 19:36:17 +0530

    Submit Commit

--- src/FixBST.cpp
+++ src/FixBST.cpp
@@ -31,7 +31,40 @@ struct node{
 	int data;
 	struct node *right;
 };
-
-void fix_bst(struct node *root){
-
+void bst_fixed(struct node*, struct node**, struct node**, struct node**, struct node**);
+void swap(int*, int*);
+void fix_bst(struct node *root)
+{
+	struct node *start, *middle, *end, *prev;
+	start = middle = end = prev = NULL;
+	bst_fixed(root, &start, &middle, &end, &prev);
+	if (start != NULL && end != NULL)
+		swap(&(start->data), &(end->data));
+	else if (start != NULL && middle != NULL)
+		swap(&(start->data), &(middle->data));
+}
+void bst_fixed(struct node* root, struct node** start, struct node** middle, struct node** end, struct node** prev)
+{
+	if (root != NULL)
+	{
+		bst_fixed(root->left, start, middle, end, prev);
+		if (*prev && root->data < (*prev)->data)
+		{
+			if (*start == NULL)
+			{
+				*start = *prev;
+				*middle = root;
+			}
+			else
+				*end = root;
+		}
+		*prev = root;
+		bst_fixed(root->right, start, middle, end, prev);
+	}
+}
+void swap(int* a, int* b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
 }
